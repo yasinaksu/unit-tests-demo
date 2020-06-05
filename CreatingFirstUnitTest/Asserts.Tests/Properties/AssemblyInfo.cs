5879637b4074ec9a689419f55aaa27137f55fc4c
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-05 19:14:22 +0300

    Assert Usage 1-2

---
+++ CreatingFirstUnitTest/Asserts.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Asserts.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Asserts.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a6082296-3c7e-4fdf-8082-3200a7f696fb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CreatingFirstUnitTest/Asserts.Tests/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Asserts.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AreEqualWithCustomErrorMessage()
+        {
+            const double givenValue = 16;
+            const double expected = 4;
+
+            double actual = Math.Sqrt(givenValue);
+            Assert.AreEqual(expected, actual, "{0} sayısının karakökü {1} olmalıdır", givenValue, expected);
+        }
+        [TestMethod]
+        public void AreEqualWithUsingDelta()
+        {
+            double expected = 3.1622;
+            //Formül : |expected-actual|<=delta
+            double delta = 0.0001;
+
+            double actual = Math.Sqrt(10);
+            Assert.AreEqual(expected, actual, delta);
+        }
+        [TestMethod]
+        public void AreEqualComparingStringsWithIgnoreCaseSensitive()
+        {
+            string expected = "HELLO";
+            string actual = "hello";
+            bool ignoreCase = true;
+            Assert.AreEqual(expected, actual, ignoreCase);
+        }
+        [TestMethod]
+        public void AreNotEqual()
+        {
+            const double notExpected = 0;
+            var actual = Math.Pow(5, 0);
+            Assert.AreNotEqual(notExpected, actual);
+        }
+
+        [TestMethod]
+        public void AreSame()
+        {
+            //Assert.AreSame() using for checking referances are same or not.
+            var numbers = new byte[] { 1, 2, 3 };
+            var numbers2 = numbers;
+            numbers[0] = 4;
+
+            Assert.AreSame(numbers, numbers2);
+        }
+
+        [TestMethod]
+        public void ComparingAreEqualAndAreSame()
+        {
+            int a = 1;
+            int b = a;
+
+            Assert.AreEqual(a, b, "AreEqual failed");
+            Assert.AreSame(a, b, "AreSame failed");
+        }
+    }
+}
