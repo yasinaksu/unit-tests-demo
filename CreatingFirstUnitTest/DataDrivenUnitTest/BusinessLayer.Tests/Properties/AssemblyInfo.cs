acd1a9a47d9111657c39656559d860716663218d
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-06 13:45:27 +0300

    data driven test demo 1

---
+++ CreatingFirstUnitTest/DataDrivenUnitTest/BusinessLayer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BusinessLayer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BusinessLayer.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("33c4bcc8-b6ac-4cc0-94fa-51f899a3b9f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CreatingFirstUnitTest/DataDrivenUnitTest/BusinessLayer.Tests/UserManagerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLayer.Tests
+{
+    [TestClass]
+    public class UserManagerTests
+    {
+        public TestContext TestContext { get; set; }
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "Users.xml",
+            "User",
+            DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void DataTest()
+        {
+            var manager = new UserManager();
+            var name = TestContext.DataRow["name"].ToString();
+            var phone = TestContext.DataRow["phone"].ToString();
+            var email = TestContext.DataRow["email"].ToString();
+            var result = manager.UserAdd(name, phone, email);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
