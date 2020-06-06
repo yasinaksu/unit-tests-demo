6f17d89ab863c9576e14cd4be859b3313b693ec9
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-06 10:22:22 +0300

    string assert methods commit

---
+++ CreatingFirstUnitTest/StringAsserts.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringAsserts.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringAsserts.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85957871-9ae5-4067-abb0-359e8788f90e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CreatingFirstUnitTest/StringAsserts.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringAsserts.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void StringContainTest()
+        {
+            StringAssert.Contains("Hello from string assert", "str");
+        }
+
+        [TestMethod]
+        public void StringMatchesTest()
+        {
+            StringAssert.Matches("Hello from string assert", new Regex(@"[a-zA-z]"));
+            StringAssert.DoesNotMatch("Hello from string assert", new Regex(@"[0-9]"));
+        }
+
+        [TestMethod]
+        public void StartsWithTest()
+        {
+            StringAssert.StartsWith("Hello from string assert", "Hello");
+        }
+
+        [TestMethod]
+        public void EndsWithTest()
+        {
+            StringAssert.EndsWith("Hello from string assert", "ert");
+        }
+    }
+}
