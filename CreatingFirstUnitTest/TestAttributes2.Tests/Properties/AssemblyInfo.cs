f1b832d61b4e977ba983523e236eac0fa7e8ae2d
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-06 14:48:02 +0300

    test attributes 2 commit

---
+++ CreatingFirstUnitTest/TestAttributes2.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAttributes2.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAttributes2.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b15b077-183e-4f53-861a-b67dd1adbf7d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CreatingFirstUnitTest/TestAttributes2.Tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAttributes2.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod,Ignore,Description("this check some data whether useful or not")]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void TestMethod2()
+        {
+            Thread.Sleep(1500);
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        [Timeout(TestTimeout.Infinite)]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
