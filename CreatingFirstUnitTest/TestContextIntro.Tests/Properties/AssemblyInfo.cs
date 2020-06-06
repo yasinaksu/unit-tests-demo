10731e751d584abb99593329382b826b8add4eef
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-06 10:45:17 +0300

    common test context usage commit

---
+++ CreatingFirstUnitTest/TestContextIntro.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestContextIntro.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestContextIntro.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("11d7dc93-a832-481e-b7c2-1745af1208d5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CreatingFirstUnitTest/TestContextIntro.Tests/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestContextIntro.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; } //this property is set automaticaly in rumtime.
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestContext.WriteLine("TestInitialize");
+            TestContext.WriteLine("Test name : {0}", TestContext.TestName);
+            TestContext.WriteLine("Test state : {0}", TestContext.CurrentTestOutcome);
+            TestContext.Properties["name"] = "Yasin Aksu";
+            TestContext.WriteLine("");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestContext.WriteLine("TestCleanup");
+            TestContext.WriteLine("Test name : {0}", TestContext.TestName);
+            TestContext.WriteLine("Test state : {0}", TestContext.CurrentTestOutcome);
+            TestContext.WriteLine("Tester Name : {0}", TestContext.Properties["name"]);
+            TestContext.WriteLine("");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestContext.WriteLine("TestMethod1");
+            TestContext.WriteLine("Test name : {0}",TestContext.TestName);
+            TestContext.WriteLine("Test state : {0}",TestContext.CurrentTestOutcome);
+            TestContext.WriteLine("Test class info : {0}",TestContext.FullyQualifiedTestClassName);
+            TestContext.WriteLine("Tester Name : {0}", TestContext.Properties["name"]);
+            TestContext.WriteLine("");
+
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
