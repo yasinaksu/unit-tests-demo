d0f49a3cfb3417e193cad4ff1f433cb12775004d
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-06 14:40:18 +0300

    Test attributes chapter 1 commit

---
+++ CreatingFirstUnitTest/TestAttributes1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAttributes1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAttributes1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a6d7ffd0-0992-4005-947e-111efe9f2c5a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CreatingFirstUnitTest/TestAttributes1.Tests/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAttributes1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod,Owner("Yasin"),TestCategory("Tester"),Priority(1)]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [Owner("Kerim")]
+        [TestCategory("Tester")]
+        [Priority(2)]
+        [TestProperty("Updater","Yasin Aksu")]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [Owner("Yasin")]
+        [TestCategory("Developer")]
+        [Priority(1)]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [Owner("Yasin")]
+        [TestCategory("Tester")]
+        [TestCategory("Developer")]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [Owner("Kerim")]
+        [TestCategory("Developer")]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
